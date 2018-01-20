6f23645608312a9684da999f5abb419e22150831
Andrei Ausianik <andrei.ausianik@iii.com>
2018-01-20 13:23:52 +0300

    closed circut task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function isCircuitClosed(matrix) {
-  // your solution here
+  matrix = matrix.map(line => line.slice());
+
+  const paint = (matrix, i, j, color) => {
+    if(i < 0 || i >= matrix.length || 
+      j < 0 || j >= matrix[0].length || 
+      matrix[i][j]) return;
+    matrix[i][j] = color;
+    paint(matrix, i - 1, j, color);
+    paint(matrix, i + 1, j, color);
+    paint(matrix, i, j - 1, color);
+    paint(matrix, i, j + 1, color);
+  }
+
+  let zone = 1;
+  for(let i = 0; i < matrix.length; i++) {
+    for(let j = 0; j < matrix[i].length; j++) {
+      if(!matrix[i][j]) {
+        paint(matrix, i, j, ++zone);
+      }
+    }
+  }
+  return zone > 2;
 }
